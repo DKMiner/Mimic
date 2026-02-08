ac76f4a8961a8ecf7c180d92faacf075ed6f4a87
DKMiner <mohamadrezadarqazvin@gmail.com>
2026-02-08 22:13:02 +0400

    forgot to update the version number, again -w-'

--- cept/Program.cs
+++ cept/Program.cs
@@ -5,7 +5,7 @@ namespace Cept
     class Program
     {
         public static string APP_NAME = "Cept! Auto Accept";
-        public static string VERSION = "2.1.0";
+        public static string VERSION = "2.2.0";
 
         private static App _instance;
 
